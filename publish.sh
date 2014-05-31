742cd53229b745478f955beab225d500e6f00abd
Jeffery Yeary <jeffery.yeary@gmail.com>
2014-05-31 10:27:47 -0400

    Commit triggered from publish script Sat May 31 10:27:47 EDT 2014

--- publish.sh
+++ publish.sh
@@ -1,6 +1,7 @@
 rm -rf ../usbsnowcrash.github.io/*
 jekyll build --destination ../usbsnowcrash.github.io/
 cd ../usbsnowcrash.github.io/
+git add -u .
 git add .
 git commit -m "Commit triggered from publish script `date`"
 git push origin master
