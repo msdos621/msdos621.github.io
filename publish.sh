644b72386e2ef1d224f4d6a8514b114926df87f0
Jeffery Yeary <jeffery.yeary@gmail.com>
2014-05-31 10:58:30 -0400

    Commit into master triggered from publish script Sat May 31 10:58:29 EDT 2014

--- publish.sh
+++ publish.sh
@@ -3,9 +3,9 @@ jekyll build --destination ../usbsnowcrash.github.io/
 cd ../usbsnowcrash.github.io/
 git add -u .
 git add .
-git commit -m "Commit triggered from publish script `date`"
+git commit -m "Commit into master triggered from publish script `date`"
 git push origin master
 cd ../blog
 git add .
-git commit -m "Commit triggered from publish script `date`"
+git commit -m "Commit into blog triggered from publish script `date`"
 git push origin blog
